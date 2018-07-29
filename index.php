6940c09b11d7c3dc08407864ea8fe696047c6947
massa <massafranck@gmail.com>
2018-07-29 14:10:39 +0200

    initial

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+ <html lang="fr">
+   <head>
+     <meta charset="utf-8" />
+     <title>Exercice4</title>
+   </head>
+   <body>
+     <p>
+<?php
+//Incréméntation de la moité de la valeur number tant que la variable n'atteint pas 10
+for($number = 1; $number <= 10; $number += 1 / 2){
+  echo $number. '<br/>';
+}
+ ?>
+</p>
+   </body>
+ </html>
